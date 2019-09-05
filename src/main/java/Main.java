da6e858442e89c8bfb9cc11adac12b0095ca58b9
Malcolm Cajuste <malcolmacajuste@gmail.com>
2019-09-05 11:02:57 -0400

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        float floatVal = 1.0f;
+        double doubleVal = 4.0d;
+        byte byteVal = 7;
+        short shortVal = 7;
+        long logVal = 5;
+
+
+        short result1 = byteVal;
+
+            System.out.println("success");
+    }
+
+}
